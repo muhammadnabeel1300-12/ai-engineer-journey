e8a611e47eea4e957e1352e3ac1d0c17dc7dba8d
Muhammad Nabeel <muhammadnabeel1300@gmail.com>
2026-09-22 20:54:30 +0500

    22/09/26 learning 1st and 2nd day

---
+++ calculator.py
@@ -0,0 +1,42 @@
+# def add(a, b):
+#     return a + b
+
+
+# def subtract(a, b):
+#     return a - b
+
+# names = ["Ali", "Ahmed", "Sara", "John"]
+# for name in names:
+#     print("Hello, " + name + "!")
+
+
+# for i in range(1, 7):
+#     for j in range(1, i):
+#         print("*",end="")
+#     print()
+
+
+
+
+# try:
+#     age = int(input("Enter your age: "))
+
+#     if age <= 0:
+#         raise ValueError("Age cannot be zero or negative.")
+
+#     print("Your age is:", age)
+
+# except ValueError as error:
+#     print("Invalid input:", error)
+
+
+with open("first.txt", "a") as file:
+    file.write("Hello, this is a sample text file.\n")
+
+print("data saved")
+
+
+with open("first.txt", "r") as file:
+    content = file.read()
+    print("File content:")
+    print(content)
---
+++ test.py
@@ -0,0 +1,31 @@
+from calculator import add, subtract
+
+add = add(10, 5)
+
+sub = subtract(20, 5)
+
+list =[1,2,3,4,5,5,6]
+
+dictio = {
+    "name": "Documents",
+    "id": 123,
+    "age": 22
+}
+
+set_data = {1, 2, 3, 4, 5}
+
+print("list:", list)
+print("dictionaries:", dictio)
+print("set_data:", set_data)
+
+
+
+
+
+
+# if add > sub:
+#     print("Addition is greater than subtraction")
+# elif add < sub:
+#     print("Subtraction is greater than addition")
+# else:
+#     print("Both are equal")
